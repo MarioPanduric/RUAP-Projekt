ae0368a309575da7cc2a3880442bc785aac7774a
MarioPanduric <mariopanduric123@hotmail.com>
2022-11-03 17:35:52 +0100

    Cherry pick

--- Aplikacija-RUAP-LV1/Aplikacija-RUAP-LV1/Program.cs
+++ Aplikacija-RUAP-LV1/Aplikacija-RUAP-LV1/Program.cs
@@ -7,7 +7,6 @@ namespace Aplikacija_RUAP_LV1
         static void Main(string[] args)
         {
             Console.WriteLine("Pozdrav iz aplikacije od Marija Pandurica");
-            Console.WriteLine("Index: 0165079118")
         }
     }
 }
